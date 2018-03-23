ca6ef274a305651ca2b72d503b29a5fdd3424f06
Ilya Yanok <ilya.yanok@gmail.com>
2018-03-23 12:56:41 +0100

    runner: limit one classifier run time by 5 seconds
    TODO: make it configurable

--- src/Result.hs
+++ src/Result.hs
@@ -3,5 +3,5 @@ module Result where
 import Data.ByteString (ByteString)
 import System.Exit
 
-data Result = Result { getResult :: (ExitCode, ByteString, ByteString)}
+data Result = Result { getResult :: Maybe (ExitCode, ByteString, ByteString)}
   deriving (Show, Eq, Ord)
--- src/Runner.hs
+++ src/Runner.hs
@@ -4,13 +4,14 @@ module Runner where
 import           Data.ByteString           (ByteString)
 import           System.Exit               (ExitCode)
 import           System.Process.ByteString (readProcessWithExitCode)
+import           System.Timeout            (timeout)
 
 import           Classifier
 import           Implementation
 
-data RawResult = RawResult {getRaw :: (ExitCode, ByteString, ByteString)}
+data RawResult = RawResult {getRaw :: Maybe (ExitCode, ByteString, ByteString)}
   deriving (Show, Eq, Ord)
 
 runOn :: Classifier -> Implementation -> IO RawResult
 runOn (Classifier fn) (ImplementationNode interpreter) =
-  RawResult <$> readProcessWithExitCode "node" [interpreter, fn] ""
+  RawResult <$> timeout 5000000 (readProcessWithExitCode "node" [interpreter, fn] "")
